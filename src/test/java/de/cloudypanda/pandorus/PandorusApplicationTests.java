d3186530a9a86862ff6513ab22703268ba5a3775
SleepyPxnda <felix@grohme.de>
2023-04-29 21:30:54 +0200

    Initial commit

---
+++ src/test/java/de/cloudypanda/pandorus/PandorusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cloudypanda.pandorus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandorusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
